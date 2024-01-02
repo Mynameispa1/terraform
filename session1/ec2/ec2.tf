16d013fb2352c6a122fe49a5c5e1671c2e178c48
Pavan Kumar Muvva <muvva.usedu1@gmail.com>
2024-01-02 13:52:05 -0600

    Terraform ec2 creation

---
+++ session1/ec2/ec2.tf
@@ -0,0 +1,36 @@
+resource "aws_instance" "myec2" {
+  ami           = "ami-03265a0778a880afb"  #devops practice
+  instance_type = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.mysg.id]
+
+  tags = {
+    Name = "Terraform"
+  }
+}
+
+resource "aws_security_group" "mysg" {
+  name        = "var.sg-name"
+  description = "var.sg-description"
+  #vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "Allow all ports"
+    from_port        = var.inbound-from-port
+    to_port          = 0
+    protocol         = "tcp"
+    cidr_blocks      = var.cidr_blocks
+    #ipv6_cidr_blocks = [aws_vpc.main.ipv6_cidr_block]
+  }
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+    tags = {
+    Name = "roboshop-all-aws"
+  }
+}
+
